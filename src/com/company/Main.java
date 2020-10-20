f80036633965c528d96e5bd2c8be2c96b2fee622
yulikflute90 <yulik-flute@mail.ru>
2020-10-20 14:45:51 +0300

    comment

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,6 +5,7 @@ public class Main {
     public static void main(String[] args) {
 	// write your code here
         System.out.println("HelloWorld");
+        System.out.println("HelloWorld");
     }
 }
 
